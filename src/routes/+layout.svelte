0fcf12bee61948c7bf5fb148cddf8fcf36ccb620
David Dal Busco <david.dalbusco@outlook.com>
2022-09-22 09:42:20 +0200

    feat: integrate blog style and main mixins

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<slot />
+
+<style lang="scss" global>
+  @use '../lib/styles/mixins/main';
+  @use '../lib/styles/mixins/doc';
+
+  @import '../lib/styles/global.scss';
+
+  :root {
+    @include main.layout;
+    @include doc.paragraphs;
+  }
+
+  article.deckgo-doc {
+    @include doc.article;
+    @include doc.spacing;
+  }
+</style>
